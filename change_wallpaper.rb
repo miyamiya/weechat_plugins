529f499c828f23df4d9a9d96b21800066d11a421
miyamiya <rai.caver@gmail.com>
2013-07-28 21:37:35 +0900

    code refactoring, and bugfix(hook miss)

--- change_wallpaper.rb
+++ change_wallpaper.rb
@@ -4,32 +4,30 @@ require 'weechat'
 
 # Setting
 DELAY = 15  # delay time (second)
-PATH = "/home/miyamiya/WallPapers" # Directory path with wallpaper
+IMGPATH = "/home/miyamiya/WallPapers" # Directory path with wallpaper
 
 # Starting time (Global)
 $now = Time.now
 
-
 def weechat_init
-  Weechat.register(
-    "change_wallpaper",
-    "miyamiya <rai.caver@gmail.com>",
-    "0.0.1",
-    "GPL3",
-    "Wallpaper will be changed if signal is received.",
-    "",
-    ""
-  )
+  Weechat.register("change_wallpaper", "miyamiya <rai.caver@gmail.com>",
+    "0.0.1", "GPL3", "Wallpaper will be changed if signal is received.",
+    "", "")
+  # Setup hook
+  Weechat.hook_signal("weechat_highlight", "do_event", "")
+  Weechat.hook_signal("weechat_pv", "do_event", "")
+  return Weechat::WEECHAT_RC_OK
 end
 
-def setup
-  Weechat.hook_signal("weechat_highlight,weechat_pv", "change_wallpicture", "")
+def do_event(data, signal, message)
+  diff = (Time.now - $now).divmod(24*60*60)
+  change_wallpicture if diff[1] > DELAY
   return Weechat::WEECHAT_RC_OK
 end
 
 def gen_picture_uri
   files = []
-  Dir::glob(PATH + "/*.{jpg,jpeg,png}").each {|f|
+  Dir::glob(IMGPATH + "/*.{jpg,jpeg,png}").each {|f|
     files << f if File.file?(f)
   }
   return files.sample
@@ -37,5 +35,6 @@ end
 
 def change_wallpicture
   path = gen_picture_uri
-  spawn("GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri 'file://#{path}'", STDERR=>STDOUT)
+  spawn("GSETTINGS_BACKEND=dconf /usr/bin/gsettings set org.gnome.desktop.background picture-uri 'file://#{path}'", STDERR=>STDOUT) if FileTest.exists?(path)
+  return Weechat::WEECHAT_RC_OK
 end
--- notify.rb
+++ notify.rb
@@ -9,30 +9,23 @@ DELAY = 15  # delay time (second)
 $now = Time.now
 
 def weechat_init
-  Weechat.register(
-    "send_notify",
-    "miyamiya <rai.caver@gmail.com>",
-    "0.0.1",
-    "GPL3",
-    "Send 'notify' if signal is received.",
-    "",
-    ""
-  )
+  Weechat.register("send_notify", "miyamiya <rai.caver@gmail.com>",
+    "0.0.1", "GPL3", "Send 'notify' if signal is received.",
+    "", "")
+
   setup
+  # Setup hook 
+  Weechat.hook_signal("weechat_highlight", "do_event", "")
+  Weechat.hook_signal("weechat_pv", "do_event", "")
   return Weechat::WEECHAT_RC_OK
 end
 
-def setup
-  Weechat.hook_signal("weechat_highlight,weechat_pv", "show_highlight", "")
-  return Weechat::WEECHAT_RC_OK
-end
-
-def show_highlight(data, signal, message)
+def do_event(data, signal, message)
   diff = (Time.now - $now).divmod(24*60*60)
   notify message if diff[1] > DELAY
+  return Weechat::WEECHAT_RC_OK
 end
 
 def notify message
   spawn("/usr/bin/notify-send", message)
 end
-
